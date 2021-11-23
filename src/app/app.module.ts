0a94b1264288f8390136efc5a1b4d5fe3ed797b3
Bohdan-Balytskyy <bbalitsky@gmail.com>
2021-11-23 22:00:27 +0200

    first version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CenzorComponent } from './cenzor/cenzor.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CenzorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
